ce5f9361a7ed100f07e9dd837174bb020d0932b3
taohui <htao@thoughtworks.com>
2019-07-21 17:07:42 +0800

    [taohui] build config client to access config for specific app and profile

---
+++ src/main/java/com/cloud/spring/configclient/ConfigController.java
@@ -0,0 +1,18 @@
+package com.cloud.spring.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+    @Value("${from}")
+    private String from;
+
+    @GetMapping("/from")
+    public String from(){
+        return this.from;
+    }
+}
